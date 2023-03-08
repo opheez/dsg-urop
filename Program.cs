52eaaff8240f3800848a2b29731d30e2a4b5b113
Ophelia Zhu <wattinytoffee@gmail.com>
2023-03-08 01:13:54 -0500

    cleanup

--- Program.cs
+++ Program.cs
@@ -2,18 +2,12 @@
 using System.Collections;
 using System.Text;
 unsafe class Program {
-    struct TableRow {
-        internal char name;
-        internal int age;
-    }
 
     public static void Main(){
 
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, World!");
         // test.debug();
-        byte[] test = new byte[100];
-        var span = new Span<byte>(test,50,10);
 
     }
 
