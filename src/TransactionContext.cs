d46f3ca70efe2ee10590a5372a0706aa1858b09f
Ophelia Zhu <wattinytoffee@gmail.com>
2023-04-25 23:08:15 -0400

    Separate read write sets into separate data structures

--- src/TransactionContext.cs
+++ src/TransactionContext.cs
@@ -8,30 +8,38 @@ public class TransactionContext {
 
     internal TransactionStatus status;
     internal uint startTxn;
-    internal Dictionary<KeyAttr, (bool, byte[]?)> RWset;
+    internal Dictionary<KeyAttr, byte[]?> Rset;
+    internal Dictionary<KeyAttr, byte[]?> Wset;
 
     public TransactionContext(uint startTxn){
         this.startTxn = startTxn;
         status = TransactionStatus.Idle;
-        RWset = new Dictionary<KeyAttr, (bool, byte[]?)>();
+        Rset = new Dictionary<KeyAttr, byte[]?>();
+        Wset = new Dictionary<KeyAttr, byte[]?>();
     }
 
     public byte[]? GetFromContext(KeyAttr keyAttr){
-        if (RWset.ContainsKey(keyAttr)){
-            return RWset[keyAttr].Item2;
+        if (Wset.ContainsKey(keyAttr)){
+            return Wset[keyAttr];
+        } else if (Rset.ContainsKey(keyAttr)){
+            return Rset[keyAttr];
         }
         return null;
     }
 
     public void SetInContext(KeyAttr keyAttr, ReadOnlySpan<byte> val, bool write){
-        RWset[keyAttr] = (write, val.ToArray());
+        if (write){
+            Wset[keyAttr] = val.ToArray();
+        } else {
+            Rset[keyAttr] = val.ToArray();
+        }
     }
 
-    public Dictionary<KeyAttr, byte[]> GetReadset(){
-        return RWset.Where(item => !item.Value.Item1).ToDictionary(item => item.Key, item => item.Value.Item2);
+    public Dictionary<KeyAttr, byte[]?> GetReadset(){
+        return Rset;
     }
-    public Dictionary<KeyAttr, byte[]> GetWriteset(){
-        return RWset.Where(item => item.Value.Item1).ToDictionary(item => item.Key, item => item.Value.Item2);
+    public Dictionary<KeyAttr, byte[]?> GetWriteset(){
+        return Wset;
     }
 }
 
