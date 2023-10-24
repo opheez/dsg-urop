ebd7280ea03cbcc8b639e797300d321f060e6dc1
Ophelia Zhu <wattinytoffee@gmail.com>
2023-10-23 22:56:07 -0400

    Use writeArray for benchmark

--- benchmark/FixedLenTableBenchmark.cs
+++ benchmark/FixedLenTableBenchmark.cs
@@ -14,20 +14,24 @@ public class FixedLenTableBenchmark : TableBenchmark
             schema[i] = (attr,sizeof(long));
         }
 
-        // one out of every group will be a read, all others are writes
-        int groupSize = (int)(1/(1-cfg.ratio));
-        int numberOfGroups = (int) cfg.datasetSize/groupSize;
-        int inserts = (groupSize - 1) * numberOfGroups;
-        if (cfg.datasetSize % groupSize != 0) {
-            int remaining = cfg.datasetSize - (groupSize * numberOfGroups);
-            inserts += remaining - 1;
+        // randomly assign reads and writes
+        int numWrites = (int)(cfg.datasetSize * cfg.ratio);
+        for (int i = 0; i < numWrites; i++){
+            long index = r.NextInt64(cfg.datasetSize);
+            // if this index is already a write, find the next available index
+            if (isWrite[(int)index]) {
+                while (isWrite[(int)index]){
+                    index += 1;
+                }
+            }
+            isWrite[(int)index] = true;
         }
 
         for (int i = 0; i < cfg.datasetSize; i++){
             values[i] = new byte[sizeof(long)];
             r.NextBytes(values[i]);
         }
-        stats = new BenchmarkStatistics($"{name}-FixedLenTableBenchmark", cfg, inserts, cfg.datasetSize);
+        stats = new BenchmarkStatistics($"{name}-FixedLenTableBenchmark", cfg, numWrites, cfg.datasetSize);
         System.Console.WriteLine("Done init");
     }
 
@@ -44,20 +48,24 @@ public class TransactionalFixedLenTableBenchmark : TableBenchmark
             schema[i] = (attr,sizeof(long));
         }
 
-        // one out of every group will be a read, all others are writes
-        int groupSize = (int)(1/(1-cfg.ratio));
-        int numberOfGroups = (int) cfg.datasetSize/groupSize;
-        int inserts = (groupSize - 1) * numberOfGroups;
-        if (cfg.datasetSize % groupSize != 0) {
-            int remaining = cfg.datasetSize - (groupSize * numberOfGroups);
-            inserts += remaining - 1;
+        // randomly assign reads and writes
+        int numWrites = (int)(cfg.datasetSize * cfg.ratio);
+        for (int i = 0; i < numWrites; i++){
+            long index = r.NextInt64(cfg.datasetSize);
+            // if this index is already a write, find the next available index
+            if (isWrite[(int)index]) {
+                while (isWrite[(int)index]){
+                    index += 1;
+                }
+            }
+            isWrite[(int)index] = true;
         }
 
         for (int i = 0; i < cfg.datasetSize; i++){
             values[i] = new byte[sizeof(long)];
             r.NextBytes(values[i]);
         }
-        stats = new BenchmarkStatistics($"{name}-TransactionalFixedLenTableBenchmark", cfg, inserts, cfg.datasetSize);
+        stats = new BenchmarkStatistics($"{name}-TransactionalFixedLenTableBenchmark", cfg, numWrites, cfg.datasetSize);
         System.Console.WriteLine("Done init");
     }
 
